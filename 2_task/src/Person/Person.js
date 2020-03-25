e68871b4b7810767cd5448f61b015e062eaf33ea
bea94k <beatakusnierz94@gmail.com>
2020-03-25 13:23:36 +0200

    Create Person component

---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Person.css';
+
+const Person = () => {
+    return (
+        <div className="person-card">
+            <p>Hello, my name is Bea, and I am 25 years old.</p>
+        </div>
+    );
+};
+
+export default Person;
