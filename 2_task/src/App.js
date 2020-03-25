50566388b3b8a12a510f714393f7f870fda0be0b
bea94k <beatakusnierz94@gmail.com>
2020-03-25 14:28:59 +0200

    Change App into class component, play with its state

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js'
 
-const App = () => {
+class App extends Component {
+  state = {
+    persons: [
+      /* using array because there are a lot of people's data */
+      { name: 'Bea', age: 25 },
+      { name: 'Mikko', age: 30 },
+      { name: 'Grzegorz Brzęczyszczykiewicz', age: 99 },
+    ]
+  };
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Hellooooooooooo</p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    )
+  }
+
+
+  /* render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Hellooooooooooo</p>
+        <Person name="Bea" age="25" />
+        <Person name="Mikko" age="30" />
+        <Person name="Grzegorz Brzęczyszczykiewicz" age="99" />
+      </div>
+    )
+  } */
+}
+
+/* const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
@@ -12,6 +49,6 @@ const App = () => {
       <Person name="Grzegorz Brzęczyszczykiewicz" age="99" />
     </div>
   );
-};
+}; */
 
 export default App;
