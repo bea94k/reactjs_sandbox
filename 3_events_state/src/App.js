cd0a3ac134f75f5804666cc71c6203dd261a08d5
bea94k <beatakusnierz94@gmail.com>
2020-03-30 11:07:17 +0300

    Add buttons and make them add, substract or restet the number

--- 3_events_state/src/App.js
+++ 3_events_state/src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   state = {
     likes: 0
-  }
+  };
+
+  addLike = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+
+  substractLike = () => {
+    this.setState({
+      likes: this.state.likes - 1
+    });
+  };
+
+  resetLikes = () => {
+    this.setState({
+      likes: this.state.likes = 0
+    });
+  };
 
   render() {
     return (
       <div>
         <h1>Total likes: {this.state.likes} </h1>
+        <button onClick={this.addLike}>Add a like</button>
+        <button onClick={this.substractLike}>Substract a like</button>
+        <button onClick={this.resetLikes}>Reset likes counter</button>
       </div>
     )
   }
