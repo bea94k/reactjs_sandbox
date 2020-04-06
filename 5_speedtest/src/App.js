aeff241a79553496cd6ffdef5ac687b2d59a6061
bea94k <beatakusnierz94@gmail.com>
2020-04-06 14:18:54 +0300

    Add generating random number, deciding the next active circle

--- 5_speedtest/src/App.js
+++ 5_speedtest/src/App.js
@@ -2,18 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import Circle from './Circle/Circle.js';
 
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/* generate random number from 1 to 4 */
+
 class App extends Component {
   state = {
-    score: 0
+    score: 0,
+    current: 0
+  };
+
+  speed = 1000;
+  timer = undefined;
+
+  /* what is the next new circle that we want to be active */
+  next = () => {
+    let nextActive = undefined;
+    /* go as long as you get a new thing, not through the whole array/elements */
+    do {
+      nextActive = getRandomNumber(1, 4);
+    } while (nextActive === this.state.current);
+    /* if the nextActive is the same as in state, run random again, stop when they are different */
+
+    this.setState({
+      current: nextActive
+    });
+
+    this.timer = setTimeout(this.next, this.speed)
+    console.log('Current random is ' + this.state.current);
   };
 
   clickHandler = circleID => {
-    console.log('I was clicked - ' + circleID);
+    console.log('Clicked cirlce number ' + circleID);
 
     this.setState({
       score: this.state.score + 1
-    })
-  }
+    });
+  };
+
+  startHandler = () => {
+    this.next();
+  };
+
+  endHandler = () => {
+    clearTimeout(this.timer);
+  };
 
   render() {
     return (
@@ -28,8 +62,8 @@ class App extends Component {
           {/* binding can pass data to the handler */}
         </main>
         <div>
-          <button>New game</button>
-          <button>End game</button>
+          <button onClick={this.startHandler}>New game</button>
+          <button onClick={this.endHandler}>End game</button>
         </div>
       </div>
     )
