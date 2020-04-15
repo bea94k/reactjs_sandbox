ef4869bbba84db67c486eb24911472e86ef6604a
bea94k <beatakusnierz94@gmail.com>
2020-04-15 13:38:28 +0300

    Add content to news card (manually and dynamically)

--- 6_news_list/src/Post/Post.js
+++ 6_news_list/src/Post/Post.js
@@ -4,10 +4,10 @@ import './Post.css';
 const Post = props => {
     return (
         <div className="news-card">
-            <h2>{props.title}niajfdskjvneifwjngv</h2>
-            <p>{props.author}niajfdskjvneifwjngv</p>
-            <p>{props.desc}niajfdskjvneifwjngv</p>
             <img src={props.img} alt={props.title} />
+            <h2>{props.title}</h2>
+            <p>{props.author}</p>
+            <p>{props.desc}</p>
         </div>
     );
 }
