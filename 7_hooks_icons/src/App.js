23b953bde432be2d792d8e30fdd780c6bec0c8ac
bea94k <beatakusnierz94@gmail.com>
2020-04-22 12:35:11 +0300

    Extract button to a separate component, connect click handlers

--- 7_hooks_icons/src/App.js
+++ 7_hooks_icons/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './Button/Button'
 
 const App = () => {
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+
+  const addLike = () => setLike(like + 1);
+  const addDislike = () => setDislike(dislike + 1);
+  const resetResult = () => {
+    setLike(0);
+    setDislike(0);
+  }
+
   return (
     <div>
-      Henlo
+      <Button click={addLike} text="Like" />
+      <Button click={addDislike} text="Dislike" />
+      <Button click={resetResult} text="Reset" />
+      <h1>Votes:</h1>
+      <p>Like: {like}</p>
+      <p>Dislike: {dislike}</p>
     </div>
   );
 }
